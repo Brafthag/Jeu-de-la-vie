ab6f9b123bd9f60259ff48937ffcf9e381690b98
Brafthag <kevin.combes@imie.fr>
2019-03-05 14:19:31 +0100

    Grosse modif

--- src/tableau.java
+++ src/tableau.java
@@ -6,71 +6,76 @@ import java.util.Random;
 
 public class tableau {
 	public static <function> void main(String[] args){
-		//les int représentés entre les crochets, représentent les lignes et colonnes
-		//du tableau à deux dimensions, respectivement
-		//int[][] tableau = new int[3][50];
-
 		
- 
-		//Parcours lignes et colonnes pour les deux for, respectivement
-		//for(int i=0; i<2; i++) 
-		//{					   
-		//	for(int j=0; j<tableau[i].length; j++)
-		//	   {
-			     //tableau[i][j] = "|" +""+j+" | "+i+"|";  
-		//	     System.out.println(tableau[i][j]); 
-		//	   }
-int tour = 0;	
-while (tour < 3){
-int compteTour = ++tour;
-System.out.print("tour : " + tour);
+		int [][] twoDim = new int [15][15];
+		
+		//twoDim[8][8] = 1; 
+		twoDim[7][9] = 1;
+		twoDim[7][10] = 1;
+		twoDim[7][11] = 1;
+		//twoDim[9][9] = 1;
+		//twoDim[9][10] = 1;
+		//twoDim[9][11] = 1;
+		//twoDim[8][12] = 1;
+		int tour = 0;
+		
+		display(twoDim);
+		
+		while (tour < 6){
+			
+				
+			int compteTour = ++tour;
+			String ligne = System.getProperty("line.separator");
+			System.out.print("tour : " + tour + ligne);
+			
+			
+			twoDim = DonneLaVie(twoDim);
+			
+			display(twoDim);
+		
+		
+		}
 
-Random rand = new Random();
-int n = rand.nextInt(19);
-int n2 = rand.nextInt(19);
-System.out.print(" premier : " +n);
-System.out.print(" 2eme : " + n2);
-
-int [][] twoDim = new int [20][20];
-
-
-int a = (twoDim.length);//5
-int b = (twoDim[0].length);//5
-
-for(int i = 0; i < a; i++){ // 1 2 3 4 5
-    for(int j = 0; j <b; j++) { // 1 2 3 4 5
-        int x = 0;
-        if(i % n == 0){ // placement des cellules de façon "aleatoire"
-        	x = 1;
-        	if (j % n2 == 0){
-        		x = 0;
-        	}
-        }
-        if ( tour >= 2){ // check le nombres de tours 
-        	//System.out.print("le resulta est : " +twoDim[0][1]);
-        	//if (twoDim[0][1] == 1){
-        			// regarde si une cellule existe a cette case presente
-		        //if(twoDim[0][2] == 0){ // regarde son voison 
-		        	if (j == 0 && i == 1){ // tue la cellule
-		        		x = 0;
+	}
+	
+	private static void display(int[][] table) {
+		for (int[] row: table) {
+			for (int cell: row) {
+				if (cell == 0) {
+					System.out.print(" " + 0 + " ");
+				} else {
+					System.out.print(" " + 1 + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	private static int[][] DonneLaVie(int[][] Eltablo) {
+		int [][] Eltablo2 = new int [15][15];
+		
+		for(int i = 0; i < Eltablo.length; i++){ // 1 2 3 4 5
+			for(int j = 0; j <Eltablo[0].length; j++) { // 1 2 3 4 5
+		        int pixelOn = 0;
+		        for (int i2 = i-1; i2 <= i+1; i2++) {
+		        	for (int j2 = j-1; j2 <= j+1; j2++) {
+		        		if (i2 >= 0 && j2 >= 0 && i2 < 15 && j2 < 15) { // Verif sort pas du tableau
+		        			if (Eltablo[i2][j2] == 1) {
+		        				pixelOn++;
+		        			}
+		        		}
 		        	}
 		        }
+		        if (pixelOn < 3 || pixelOn > 4) {
+		        	Eltablo2[i][j] = 0;
+		        } else {
+		        	Eltablo2[i][j] = 1;
+		        }
 		        
-        
-        twoDim[i][j] = x;
-        if (x == x) {
-            System.out.print(" " + x + " ");
-        } else {
-        	x = 1;
-            System.out.print(x + " ");
-        }
-    }//end of for J
-    System.out.println();
-}//end of for i
-}// end of while
-
-
+			}
 		}
-		
+		return Eltablo2;	        
+	}
+}
 
-		}
